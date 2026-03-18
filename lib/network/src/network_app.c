81d8dc5f24abe4e5837e6f6a61138b53a8d42adf
SimoPela <simone.pelascini@outlook.com>
2026-03-18 13:57:22 +0100

    [flag] copyright added to new files

--- lib/network/src/network_app.c
+++ lib/network/src/network_app.c
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2026 Simone Pelascini and Aurélien Bollin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+
+
 #include "network_app.h"
 #include "app_config.h"
 
--- lib/network/src/network_app.h
+++ lib/network/src/network_app.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2026 Simone Pelascini and Aurélien Bollin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+
+
 #ifndef NETWORK_APP_H
 #define NETWORK_APP_H
 
