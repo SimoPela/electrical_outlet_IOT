f671af2b66b91a6a6cead792ebdb85db0bfeda92
SimoPela <simone.pelascini@outlook.com>
2026-03-21 10:44:33 +0100

    [new] new folder herarchy inside lib

--- lib/sensirion/include/sensirion_i2c_hal.h
+++ lib/sensirion/include/sensirion_i2c_hal.h
@@ -1,3 +1,34 @@
+/*
+ * Copyright (c) 2018, Sensirion AG
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Sensirion AG nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef SENSIRION_I2C_HAL_H
 #define SENSIRION_I2C_HAL_H
 
@@ -23,12 +54,12 @@ int16_t sensirion_i2c_hal_select_bus(uint8_t bus_idx);
  * Initialize all hard- and software components that are needed for the I2C
  * communication.
  */
-int16_t sensirion_i2c_hal_init(int gpio_sda, int gpio_scl);
+void sensirion_i2c_hal_init(void);
 
 /**
  * Release all resources initialized by sensirion_i2c_hal_init().
  */
-int16_t sensirion_i2c_hal_free(void);
+void sensirion_i2c_hal_free(void);
 
 /**
  * Execute one read transaction on the I2C bus, reading a given number of bytes.
@@ -40,7 +71,7 @@ int16_t sensirion_i2c_hal_free(void);
  * @param count   number of bytes to read from I2C and store in the buffer
  * @returns 0 on success, error code otherwise
  */
-int16_t sensirion_i2c_hal_read(uint8_t address, uint8_t* data, uint16_t count);
+int8_t sensirion_i2c_hal_read(uint8_t address, uint8_t* data, uint8_t count);
 
 /**
  * Execute one write transaction on the I2C bus, sending a given number of
@@ -53,8 +84,8 @@ int16_t sensirion_i2c_hal_read(uint8_t address, uint8_t* data, uint16_t count);
  * @param count   number of bytes to read from the buffer and send over I2C
  * @returns 0 on success, error code otherwise
  */
-int16_t sensirion_i2c_hal_write(uint8_t address, const uint8_t* data,
-                               uint16_t count);
+int8_t sensirion_i2c_hal_write(uint8_t address, const uint8_t* data,
+                               uint8_t count);
 
 /**
  * Sleep for a given number of microseconds. The function should delay the
