5ca9fef09dc2bdc1cd8f2fe17c52f87f0f9bbf15
SimoPela <simone.pelascini@outlook.com>
2026-03-23 13:28:34 +0100

    [new] pinout esp32 updated

--- lib/config/include/esp32_pinout.h
+++ lib/config/include/esp32_pinout.h
@@ -26,6 +26,8 @@
  */
  #define PIN_PMS7003_TX_SENSOR   GPIO_NUM_16   // Sensor TX -> ESP32 RX
  #define PIN_PMS7003_RX_SENSOR   GPIO_NUM_17   // Sensor RX -> ESP32 TX
+ #define PIN_PMS7003_SET         GPIO_NUM_9
+ #define PIN_PMS7003_RESET       GPIO_NUM_10
 
 /*
  * ============================================================
